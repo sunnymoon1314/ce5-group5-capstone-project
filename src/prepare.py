54fac6795952b42a1b9f5ddd028135ed6ac9fc12
sunnymoon1314 <moonysun1314@gmail.com>
2024-05-18 09:59:36 +0800

    Updated ci.yml.

--- src/prepare.py
+++ src/prepare.py
@@ -4,10 +4,12 @@ import pandas as pd
 def prepare():
     # Load dataset.
     data = datasets.load_iris()
-    print(data)
+    # print(data)
 
     # https://stackoverflow.com/questions/55346510/how-do-i-write-scikit-learn-dataset-to-csv-file.
     df = pd.DataFrame(data=data['data'], columns = data['feature_names'])
+    df.head(10)
+    df.tail(10)
     df.to_csv('data\iris.txt', sep = ',', index = False)
 
 if __name__ == "__main__":
